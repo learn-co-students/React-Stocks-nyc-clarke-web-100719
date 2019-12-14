b019359f04ad34eb589f0cc455a00c0d817ec37f
DannyBrito <britodanny1997@gmail.com>
2019-12-14 18:24:36 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,89 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  
+  state={
+    fetchStocks: [],
+    catFilter: 'All',
+    sortTerm: null,
+    portfolio:[]
+  }
+
+  fetchStocks = () =>{
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(json =>{
+      this.setState({
+        fetchStocks: [...json]
+      })
+    })
+  }
+
+  handleFilter = (filter) =>{
+      this.setState({
+        catFilter: filter
+      })
+  }
+  handleSort = (term) => {
+    console.log(term)
+    this.setState({
+      sortTerm: term
+    })
+  }
+
+  componentDidMount(){
+    this.fetchStocks()
+  }
+
+  filterData =()=>{
+    let newData =[...this.state.fetchStocks]
+    if(this.state.catFilter !== "All") newData = newData.filter(stock => stock.type === this.state.catFilter)
+    switch(this.state.sortTerm){
+      case "Alphabetically":
+        newData.sort((a,b)=> a.name.localeCompare(b.name))
+        break;
+      case "Price":
+        newData.sort((a,b)=> a.price-b.price)
+        break;
+        default:
+        break;
+    }
+    return newData
+  }
+
+  getStock = (id) =>{
+    if(![...this.state.portfolio].find(st => st.id === id)){
+    let stock = [...this.state.fetchStocks].find(st => st.id === id)
+    this.setState({
+      portfolio: [...this.state.portfolio,stock]
+    })
+    }
+  }
+
+  sellStock = (id) =>{
+    let portfolio = [...this.state.portfolio]
+    let ind =portfolio.findIndex(st => st.id === id)
+    portfolio.splice(ind,1)
+    this.setState({
+      portfolio
+    })
+  }
 
   render() {
+    let finalStocks = this.filterData()
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleFilter={this.handleFilter} currentSort={this.state.sortTerm} handleSort={this.handleSort}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={finalStocks} handleClick={this.getStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <StockContainer stocks={this.state.portfolio} handleClick={this.sellStock}/>
 
             </div>
           </div>
