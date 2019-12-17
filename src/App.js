f55af3fff6fca7d1dedbb3274a6279d20aebb5fb
jastaffen <jack.staffen@gmail.com>
2019-12-17 18:04:02 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
+const API = 'http://localhost:3000/stocks';
+
 class App extends Component {
+
+  state = {
+    stocks: [],
+    portfolio: []
+  }
+
+  addToPortfolio = (stock) => {
+    this.setState({
+      portfolio: [...this.state.portfolio, stock]
+    })
+  }
+
+  sellStock = (stock) => {
+    let portFolioWithoutSelectedStock = this.state.portfolio.filter(portStock => portStock.id !== stock.id)
+    this.setState({
+      portfolio: portFolioWithoutSelectedStock
+    })
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(stocks => this.setState({
+      stocks: stocks
+    }))
+  }
+
   render() {
     return (
       <div>
         <Header/>
-        <MainContainer/>
+        <MainContainer stocks={this.state.stocks} addToPortfolio={this.addToPortfolio} portfolio={this.state.portfolio} sellStock={this.sellStock} />
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,13 +3,30 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  state = {
+    portfolio: false
+  }
+
+  renderStocks = () => {
+    let stocksToDisplay = this.props.stocks;
+    if (this.props.sort === 'Alphabetically') {
+      stocksToDisplay = stocksToDisplay.sort((stockA, stockB) => stockA.ticker.localeCompare(stockB.ticker))
+    } else if (this.props.sort === 'Price') {
+      stocksToDisplay = stocksToDisplay.sort((stockA, stockB) => stockB.price - stockA.price)
+    }
+    
+    if (this.props.filter) {
+      stocksToDisplay = stocksToDisplay.filter(stock => stock.type === this.props.filter)
+    }
+    
+    return stocksToDisplay.map(stock => <Stock stock={stock} key={stock.id} addToPortfolio={this.props.addToPortfolio} portfolio={this.state.portfolio} />)
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.renderStocks()}
       </div>
     );
   }
